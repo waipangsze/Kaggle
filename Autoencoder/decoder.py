10b0353a68f47c6ac97da2774edfb28e6e6f8e38
waipangsze <waipangsze@gmail.com>
2017-12-30 18:51:14 +0800

    Add files via upload

--- Autoencoder/decoder.py
+++ Autoencoder/decoder.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 # download the mnist to the path '~/.keras/datasets/' if it is the first time to be called
 # X shape (60 000, 28x28), y shape (10 000, )
 # (x_train, _), (x_test, y_test) = mnist.load_data()
-x_train = np.loadtxt(open("data3.csv"), delimiter=',')
+x_train = np.loadtxt(open("data4.csv"), delimiter=',')
 
 # data pre-processing
 x_train = x_train.astype('float32') / 255.       # minmax_normalized
@@ -54,10 +54,10 @@ decoder = Model(input=encoded_input, output=decoder_layer4(decoder_layer3(decode
 autoencoder.compile(optimizer='adam', loss='mse')
 
 # training, fit(X, Y), X and Y are same vector
-autoencoder.fit(x_train, x_train, nb_epoch=50, batch_size=50, shuffle=True)
+autoencoder.fit(x_train, x_train, nb_epoch=300, batch_size=200, validation_split=0.1, shuffle=True)
 
-autoencoder.save('autoencoder.h5')
-encoder.save('encoder.h5')
+# autoencoder.save('autoencoder.h5')
+# encoder.save('encoder.h5')
 decoder.save('decoder.h5')
 # plot network
 # plot_model(model, to_file='model.png')
