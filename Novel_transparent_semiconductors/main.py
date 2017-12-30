d9e24f726fd591168955da9b6f18c268c28c7691
waipangsze <waipangsze@gmail.com>
2017-12-30 17:38:27 +0800

    Add files via upload

--- Novel_transparent_semiconductors/main.py
+++ Novel_transparent_semiconductors/main.py
@@ -5,27 +5,22 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.optimizers import SGD, RMSprop, Adam
 from keras.callbacks import ModelCheckpoint
-import pre_processing
 from pandas import Series, DataFrame
+from keras.models import Model
+from keras.layers import Input
+import pre_processing
+import load_model
 
-x_train, y_train, test, m = pre_processing.load_data()
+x_train, y_train, m = pre_processing.load_train_data()
 
-# Dense(64) is a fully-connected layer with 64 hidden units.
-# in the first layer, you must specify the expected input data shape:
-# here, 20-dimensional vectors.
-# Dense(): fully connected layer.
-model = Sequential()
-model.add(Dense(64, activation='relu', input_dim=m))
-model.add(Dropout(0.5))
-model.add(Dense(164, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(2, activation='relu'))
+model = load_model.load_ann(m)
 
+# optimization
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 rmsp = RMSprop(lr=0.001, rho=0.9, epsilon=None, decay=0.0)
 adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0)
-# sgd, adam
-model.compile(loss='mae', optimizer=sgd, metrics=['msle'])
+
+model.compile(loss='msle', optimizer=sgd, metrics=['msle'])
 
 # define the checkpoint
 filepath="./weights/weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
@@ -33,7 +28,7 @@ checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=0, save_best_only
 callbacks_list = [checkpoint]
 
 # Train the model, iterating on the data in batches of 128 samples
-history = model.fit(x_train, y_train, validation_split =0.1, epochs=100, batch_size=50, callbacks=callbacks_list)
+history = model.fit(x_train, y_train, validation_split =0.1, epochs=10, batch_size=50, callbacks=callbacks_list)
 # plot metrics
 '''
 pyplot.plot(history.history['mean_squared_error'])
